e25f5044648173b4e147451d5b65d09ae5bfbe4b
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 04:28:20 +0530

    profile view created | flash message edited

--- blog_template_container/users/views.py
+++ blog_template_container/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
@@ -12,9 +12,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created For {username}!')
-            return redirect('website_home')
+            messages.success(request, f'HI,{username}! Login')
+            return redirect('login_view')
     else:
         form = UserRegistrationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
