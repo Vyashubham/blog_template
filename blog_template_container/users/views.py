96cb87325bfeef46f9f35f31b0c1b8f772e9c8a8
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 03:51:34 +0530

    switched to UserRegistration Form

--- blog_template_container/users/views.py
+++ blog_template_container/users/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created For {username}!')
             return redirect('website_home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
 
     return render(request, 'users/register.html', {'form': form})
