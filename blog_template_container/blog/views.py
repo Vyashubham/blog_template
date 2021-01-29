3efc8698d8334054fce61af3ed9c4f67f79f2812
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 01:37:35 +0530

    Post model added --> connected with templates

--- blog_template_container/blog/views.py
+++ blog_template_container/blog/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 #fake posts Dummy_Data
-posts = [
-    {
-        'title': 'title1',
-        'content': 'abcdefghijklmnopqrstuvwxyz',
-        'date': 'Jan 28, 2021'
+# posts = [
+#     {
+#         'title': 'title1',
+#         'content': 'abcdefghijklmnopqrstuvwxyz',
+#         'date': 'Jan 28, 2021'
 
-    }
-]
-
-
+#     }
+# ]
 
 
 def home(request):
+
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
+
+
     return render(request, 'blog/index.html', context)
 
 
