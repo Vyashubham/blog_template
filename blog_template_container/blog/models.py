0a37abbd5f03c6c36d5fd7947ebb1bb036191b3c
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 01:13:49 +0530

    Post model added

--- blog_template_container/blog/models.py
+++ blog_template_container/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    Title = models.CharField(max_length=100)
+    Content = models.TextField()
+    Date = models.DateTimeField(auto_now_add=True)
+    Last_Modified = models.DateTimeField(auto_now=True)
