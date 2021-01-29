75a1fe29e1ea1999e406ba684d3c566b6f4f4b20
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 01:27:15 +0530

    dunder method added for post model --> shows title for every blog

--- blog_template_container/blog/models.py
+++ blog_template_container/blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     Content = models.TextField()
     Date = models.DateTimeField(auto_now_add=True)
     Last_Modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.Title
