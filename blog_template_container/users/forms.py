f885a4c7d4f60f300d0a63c81e614d7d20f14091
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-01-30 19:04:45 +0530

    users Profile model edited --> editing capabilites added to frontend

--- blog_template_container/users/forms.py
+++ blog_template_container/users/forms.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django.forms import fields
-
-
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,3 +11,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
