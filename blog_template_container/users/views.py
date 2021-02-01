80d6d03053307560eeaa7ea34d842dea7460d2fe
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-02-01 05:58:33 +0530

    reverting to stable state 6319a33 --> sha

--- blog_template_container/users/views.py
+++ blog_template_container/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -21,21 +21,4 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, 'Changes Saved')
-            return redirect('profile')
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/profile.html')
