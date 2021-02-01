0fc817a07ff1e28fe955e0c246e48325c3f91a40
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-02-01 08:11:05 +0530

    switched to class based views --> home view updated | curd views added

--- blog_template_container/blog/views.py
+++ blog_template_container/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.contrib.auth.models import User
+
 
 #fake posts Dummy_Data
 # posts = [
@@ -13,14 +17,54 @@ from .models import Post
 # ]
 
 
-def home(request):
+# def home(request):
 
-    context = {
-        'posts': Post.objects.all()
-    }
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/index.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-Date']
+
+    paginate_by = 2 #change this to a higher number
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-Date']
+
+    paginate_by = 2 #change this to a higher number
+
+    def get_query_set(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects(username=user).order_by('-Date')
 
 
-    return render(request, 'blog/index.html', context)
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['Title', 'Content']
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['Title', 'Content']
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    fields = ['Title', 'Content']
+
+    success_url = '/'
+
 
 
 def about(request):
