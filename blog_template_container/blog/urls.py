b9ab9882267d29db147d73ffbcab1782655becf1
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-02-01 08:09:57 +0530

    create, update, read, delete views urls mapped --> switched to class based views

--- blog_template_container/blog/urls.py
+++ blog_template_container/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 urlpatterns = [
-    path('', views.home, name="website_home"),
+    # path('', views.home, name="website_home"),
+    path('', PostListView.as_view(), name="website_home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail_view"),
+    path('post/new/', PostCreateView.as_view(), name="post_create_view"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update_view"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete_view"),
+    path('user/<str:username>', UserPostListView.as_view(), name="user_detail_view"),
     path('about/', views.about, name="about"),
 ]
