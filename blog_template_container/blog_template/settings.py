9748b2e70ed41f7286be55097b7191014ace3bd1
shubham <shubhamvyas.indore@gmail.com>
2021-02-04 05:46:17 +0530

    err fix try-2

--- blog_template_container/blog_template/settings.py
+++ blog_template_container/blog_template/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
