1768d5c791075060a0133ed3401e35290d2f72a0
shubham <shubhamvyas.indore@gmail.com>
2021-02-07 05:38:11 +0530

    Post model udpated --> slug field added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from tinymce.models import HTMLField
+from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -9,7 +10,11 @@ class Post(models.Model):
     Content = HTMLField()
     Date = models.DateTimeField(auto_now_add=True)
     Last_Modified = models.DateTimeField(auto_now=True)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.Title)
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.Title
