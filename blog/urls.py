0b36c2dabbb95ecc361ed1a8660dbf73195d7778
shubham <shubhamvyas.indore@gmail.com>
2021-02-07 05:38:51 +0530

    slug variable passed in --->replaced pk (post id)

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # path('', views.home, name="website_home"),
     path('', PostListView.as_view(), name="website_home"),
     path('post/<slug:slug>/', PostDetailView.as_view(), name="post_detail_view"),
-    path('post/new/', PostCreateView.as_view(), name="post_create_view"),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update_view"),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete_view"),
+    path('new/', PostCreateView.as_view(), name="post_create_view"),
+    path('post/<slug:slug>/update/', PostUpdateView.as_view(), name="post_update_view"),
+    path('post/<slug:slug>/delete/', PostDeleteView.as_view(), name="post_delete_view"),
     path('user/<str:username>', UserPostListView.as_view(), name="user_detail_view"),
     path('about/', views.about, name="about"),
 ]
