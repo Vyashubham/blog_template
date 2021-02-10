d943cf6a1100332e485c398e7fdd87c89b07be41
Shubham Vyas <shubhamvyas.indore@gmail.com>
2021-02-11 01:27:46 +0530

    VyasProfile model created --> cv listing model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from tinymce.models import HTMLField
 
 class Profile(models.Model):
 
@@ -12,3 +12,17 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
+
+class VyasProfile(models.Model):
+
+    Title = models.CharField(max_length=100)
+    Description = HTMLField()
+    Date_started = models.DateField()
+    Date_ended = models.DateField(null=True)
+    Category = models.CharField(max_length=50)
+    
+    
+
+    def __str__(self):
+        return f'{self.Title}'
